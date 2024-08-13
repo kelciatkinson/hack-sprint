33a2d92e570f11e253158f0ed5ea961e31582c43
Jeremy Mitts <jermitts@gmail.com>
2024-08-13 17:57:34 -0500

    api update

--- api.mjs
+++ api.mjs
@@ -44,7 +44,7 @@ const requestListener = async function (req, res) {
         //if (req.query.pull != undefined) {
           const jsonString = util.inspect(req);
           console.log(jsonString);
-          res.end(jsonString);
+          res.end("OUTPUT" + jsonString);
         //}
       } else {
         res.end(readFromFile('data.json'));
