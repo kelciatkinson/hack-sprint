6fb2fa2304a756c1680d9adba9b42217242994c8
Jeremy Mitts <jermitts@gmail.com>
2024-08-13 16:43:23 -0500

    api update

--- api.mjs
+++ api.mjs
@@ -39,7 +39,7 @@ const requestListener = async function (req, res) {
         res.end(JSON.stringify({characterResponse}));
       });
     } else {
-      if ((req.method == 'GET') && (req.query != undefined))
+      if ((req.method == 'GET') && (req.query != undefined)) {
         if (req.query.pull != undefined) {
           res.end("pull would happen here");
         }
