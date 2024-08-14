165df160049fd7df4077a57753c71c463b22bcfd
Jeremy Mitts <jermitts@gmail.com>
2024-08-14 01:04:53 -0500

    correction to ai parts

--- api.mjs
+++ api.mjs
@@ -44,10 +44,13 @@ const requestListener = async function (req, res) {
         data += chunk.toString();
       });
       req.on('end', async () => {
-        console.log('POST data:', data);
         data = JSON.parse(data);
-//        let characterResponse = await getData(data.characterName, data.characterBio, data.userMessage);
-        let characterResponse = "I'd love to chat with you... maybe another time!";
+
+        console.log("userMessage:       ", data.userMessage);
+
+        const characterResponse = await getData(data.characterName,
+          data.characterBio, data.userMessage);
+
         res.writeHead(200, headers);
         res.end(JSON.stringify({characterResponse}));
         characterResponse = await getData(data.characterName, data.characterBio, data.userMessage);
@@ -78,12 +81,10 @@ server.listen(port, host, () => {
 async function getData(characterName, characterBio, userMessage) {
   const client = new BedrockRuntimeClient(AWS_config);
   const modelId = "anthropic.claude-3-haiku-20240307-v1:0";
-  const conversation = [{
-      role: "user",
-      content: [{ text: userMessage }]
-    }];
-  let systemText = [{text: `You are ${characterName} on a dating site, having a conversation with a user looking for a potential relationship. Here is ${characterName}'s bio: '${characterBio}'`}]
-  systemText = [{text: "Your name is Alladin. You're a nice guy!"}];
+  const conversation = [{ role: "user", content: [{ text: userMessage }],},];
+  let systemText = [{text: `You are ${characterName} on a dating site, ` +
+    `having a conversation with a user looking for a potential ` +
+    `relationship. Here is ${characterName}'s bio: '${characterBio}'`}];
   try {
     const command = new ConverseCommand({ modelId, system: systemText, messages: conversation });
     const response = await client.send(command);
